57da526790088e87e53fc48100295cd32012ebda
vikashgitkit <vikashofficial9@gmail.com>
2025-07-18 20:03:50 +0530

    Todo app: delete input box

--- JAVASCRIPT/Projects/Todo app/todo.js
+++ JAVASCRIPT/Projects/Todo app/todo.js
@@ -1,39 +1,38 @@
 let todoList = [
-  { item: "Buy Milk", dueDate: "4/10/2025" },
-  { item: "Go to college", dueDate: "5/11/2025" },
+  {
+    item: 'Buy Milk',
+    dueDate: '4/10/2023'
+  },
+  {
+    item: 'Go to College',
+    dueDate: '4/10/2023'
+  }
 ];
+
 displayItems();
+
 function addTodo() {
-  let inputElement = document.querySelector("#todo-input");
-  let dateElement = document.querySelector("#todo-date");
+  let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
   let todoItem = inputElement.value;
   let todoDate = dateElement.value;
   todoList.push({item: todoItem, dueDate: todoDate});
-  inputElement.value = "";
+  inputElement.value = '';
   dateElement.value = '';
-
   displayItems();
 }
 
 function displayItems() {
-  let containerEle = document.querySelector(".todo-container");
-  let newHtml = "";
+  let containerElement = document.querySelector('.todo-container');
+  let newHtml = '';
   for (let i = 0; i < todoList.length; i++) {
-    // let item = todoList[i].item;
-    // let dueDate = todoList[i].dueDate;
-
-    //sorthand way by of destructuring
     let {item, dueDate} = todoList[i];
     newHtml += `
-        <div>
-        <span> ${item} </span>
-        <span> ${dueDate} </span>
-
-        <button onclick="todoList.splice(${i}, 1);
-        displayItems();
-        ">Delete</button>
-        </div>
-        `;
+      <span>${item}</span>
+      <span>${dueDate}</span>
+      <button class='btn-delete' onclick="todoList.splice(${i}, 1);
+      displayItems();">Delete</button>
+    `;
   }
-  containerEle.innerHTML = newHtml;
+  containerElement.innerHTML = newHtml;
 }

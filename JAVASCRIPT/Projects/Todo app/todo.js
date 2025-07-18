0e882402895f301616580ad76ddcdf69d648c7db
vikashgitkit <vikashofficial9@gmail.com>
2025-07-18 19:34:51 +0530

    Todo app: input box

--- JAVASCRIPT/Projects/Todo app/todo.js
+++ JAVASCRIPT/Projects/Todo app/todo.js
@@ -17,11 +17,13 @@ function displayItems(){
     for(let i = 0; i < todoList.length; i++){
         newHtml += `
         <div>
-        <span>${todoList[i]} </span>
-        <button>Delete</button>
+        <span> ${todoList[i]} </span>
+        <button onclick="todoList.splice(${i}, 1);
+        displayItems();
+        ">Delete</button>
         </div>
         `;
 
-        containerEle.innerHTML = newHtml;
     }
+    containerEle.innerHTML = newHtml;
 }

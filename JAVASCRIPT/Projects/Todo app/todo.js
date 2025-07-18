dfc25a2e52fc7899ac73fec0369d63ee70f8d239
vikashgitkit <vikashofficial9@gmail.com>
2025-07-18 18:43:37 +0530

    Todo app: input box

--- JAVASCRIPT/Projects/Todo app/todo.js
+++ JAVASCRIPT/Projects/Todo app/todo.js
@@ -1,6 +1,16 @@
+let todoList = [];
 function addTodo(){
     let inputElement = document.querySelector('#todo-input');
      let todoItem = inputElement.value;
      console.log(todoItem);
+     todoList.push(todoItem);
+     inputElement.value = '';
      
+}
+
+function displayItems(){
+    let displayEle = document.querySelector('#todo-items');
+    for(let i = 0; i < todoList.length; i++){
+        displayEle.innerText = displayEle.innerText + todoList[i];
+    }
 }

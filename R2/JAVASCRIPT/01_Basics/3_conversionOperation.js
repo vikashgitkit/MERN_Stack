ecda229a58ddded18abad50636a308ef23c224ae
vikashgitkit <vikashofficial9@gmail.com>
2025-07-22 17:08:13 +0530

    prefix and postfix

--- R2/JAVASCRIPT/01_Basics/3_conversionOperation.js
+++ R2/JAVASCRIPT/01_Basics/3_conversionOperation.js
@@ -33,10 +33,36 @@ let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
 
 let someNum = 33;
 let stringNum = String(someNum);
-console.log(typeof stringNum); //string
-console.log(stringNum); //33
+// console.log(typeof stringNum); //string
+// console.log(stringNum); //33
+
 
 // ******************** Operations *******************
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(3+4*5%3); //this is way not recommended, instead should use () so it will execute step by step like below
+// console.log((3+4)*(5%3));
+
+
+//-------------- Prefix and Postfix------//
+// ✅ 1. Postfix (i++ / i--):
+//Use the value first, then increment/decrement it.
+
+let i = 5;
+let result = i++;  // result = 5, i becomes 6
+console.log(result, i);
+
+// ✅ 2. Prefix (++i / --i):
+// Increment/decrement first, then use the value.
+let j = 5;
+let inc = ++j;  // j becomes 6, inc = 6
+console.log(inc, j);
+
+
+
 
 
 

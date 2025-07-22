8f0c35b205e4c600be81f74fc5b66745675ad591
vikashgitkit <vikashofficial9@gmail.com>
2025-07-22 12:47:53 +0530

    primitive data type

---
+++ R2/JAVASCRIPT/01_Basics/2_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //Treat all JS code as newer version 
+
+//alert (3+4); //when run "node 2_dataTypes.js" will through an error bcz here we are using nodejs,not browser
+
+//Primitive data type
+let str = "string";
+let age = 12; //number
+let isLoggedIn = false; //boolean
+let state; //undefined: means we didn't assign the value
+let weather = null;//standalone value: means nothing get from the api or somewhere
+//symbole: unique
+
+//Object

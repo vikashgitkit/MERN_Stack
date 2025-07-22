986557abc40bd795ce9c3740ad02418781f90701
vikashgitkit <vikashofficial9@gmail.com>
2025-07-22 17:21:42 +0530

    comparison

---
+++ R2/JAVASCRIPT/01_Basics/4_comparison.js
@@ -0,0 +1,8 @@
+console.log("2">1); //true
+
+//Comparisons convert null into a number 0, treating is as 0. Below 3 consoles concept we say "type coercion"
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+
+

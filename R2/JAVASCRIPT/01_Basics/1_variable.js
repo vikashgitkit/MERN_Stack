5a51f42dcd757d188d57d219d22083b7619cc3f2
vikashgitkit <vikashofficial9@gmail.com>
2025-07-22 12:10:10 +0530

    constant

---
+++ R2/JAVASCRIPT/01_Basics/1_variable.js
@@ -0,0 +1,8 @@
+const accountId = 144523;
+let accountEmail = "steve@google.com";
+var accountPassword = "12345";
+accountCity = "Barmingham";
+
+//accountId = 4; //Will through an error bcz We can't change the const value
+
+console.log();

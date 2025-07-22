04a578aafdf3d64f4f5a0abd2ed468ac32fd07ea
vikashgitkit <vikashofficial9@gmail.com>
2025-07-22 16:23:07 +0530

    number to boolean

---
+++ R2/JAVASCRIPT/01_Basics/3_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33";
+let score1 = "33abc";
+let score2 = null
+let score3 = undefined
+
+// console.log(typeof(score));//string
+//convert string into number
+const valueInNumber = Number(score);
+const valueInNumber1 = Number(score1);
+const valueInNumber2 = Number(score2);
+const valueInNumber3 = Number(score3);
+
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber1); //NaN
+// console.log(valueInNumber2); //0
+// console.log(valueInNumber); //NaN
+
+//------------------------------//
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); //true
+
+
+
+

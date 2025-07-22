e3397b7941ceefe6d57b1a244d3e0f4a886b5772
vikashgitkit <vikashofficial9@gmail.com>
2025-07-22 12:15:18 +0530

    console.table

--- R2/JAVASCRIPT/01_Basics/1_variable.js
+++ R2/JAVASCRIPT/01_Basics/1_variable.js
@@ -3,6 +3,11 @@ let accountEmail = "steve@google.com";
 var accountPassword = "12345";
 accountCity = "Barmingham";
 
+//let's try to update the values
 //accountId = 4; //Will through an error bcz We can't change the const value
+accountEmail = "sj@gmail.com";
+accountPassword = "8833";
+accountCity = "London";
 
-console.log();
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);

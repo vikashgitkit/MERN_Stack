32e2d4cf343a9b825d73f2d8c629991dd9528919
vikashgitkit <vikashofficial9@gmail.com>
2025-07-23 21:52:58 +0530

    string methods

--- R2/JAVASCRIPT/01_Basics/5_string.js
+++ R2/JAVASCRIPT/01_Basics/5_string.js
@@ -3,10 +3,16 @@ const age = 26;
 
 console.log(`Hello My name is ${name} and age is ${age}`);
 
-const gameName = new String('Barmingham');
+const gameName = new String('Barmingham');//create obj of string
 
 console.log(gameName[0]);//B
 console.log(gameName.__proto__);//{}
-console.log(gameName.length);//10
+console.log(gameName.length);//
+console.log(gameName.toUpperCase());//BARMINGHAM
+console.log(gameName.charAt(2));//r
+console.log(gameName.indexOf('r'));//2
+
+
+
 
 

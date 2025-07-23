521e622e97d583fc18fbd5ea37ebf38fbca555c4
vikashgitkit <vikashofficial9@gmail.com>
2025-07-23 21:47:05 +0530

    string

--- R2/JAVASCRIPT/01_Basics/5_string.js
+++ R2/JAVASCRIPT/01_Basics/5_string.js
@@ -2,3 +2,11 @@ const name = "Steve";
 const age = 26;
 
 console.log(`Hello My name is ${name} and age is ${age}`);
+
+const gameName = new String('Barmingham');
+
+console.log(gameName[0]);//B
+console.log(gameName.__proto__);//{}
+console.log(gameName.length);//10
+
+

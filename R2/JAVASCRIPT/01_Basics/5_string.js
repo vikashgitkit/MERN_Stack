e64cdfce5a182653f23f4c2dd81cbf680123ea5b
vikashgitkit <vikashofficial9@gmail.com>
2025-07-23 22:01:14 +0530

    trim function

--- R2/JAVASCRIPT/01_Basics/5_string.js
+++ R2/JAVASCRIPT/01_Basics/5_string.js
@@ -13,6 +13,21 @@ console.log(gameName.charAt(2));//r
 console.log(gameName.indexOf('r'));//2
 
 
+const newStr = gameName.substring(0,4);
+console.log(newStr);//Barm
+
+const sliceStr = gameName.slice(-8, 4);
+console.log(sliceStr);//rm
+
+const trimStr = "    Steve    ";
+console.log(trimStr);
+console.log(trimStr.trim());//trim remove space from start and end
+
+
+
+
+
+
 
 
 

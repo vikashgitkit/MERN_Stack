f504f67e60a0fd75ffe3473e03f7feadfb86b7aa
vikashgitkit <vikashofficial9@gmail.com>
2025-07-24 16:52:23 +0530

    Date object

---
+++ R2/JAVASCRIPT/01_Basics/7_dates.js
@@ -0,0 +1,14 @@
+const myDate = new Date();
+console.log(myDate.toString());//Thu Jul 24 2025 16:50:33 GMT+0530 (India Standard Time)
+console.log(myDate.toDateString());//Thu Jul 24 2025
+console.log(myDate.toLocaleDateString());//7/24/2025
+console.log(myDate.toLocaleString());//7/24/2025, 4:50:33 PM
+console.log(myDate.toLocaleTimeString());//4:50:33 PM
+console.log(myDate.getTimezoneOffset());//-330
+console.log(myDate.getUTCFullYear());//2025
+
+
+
+
+
+

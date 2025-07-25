c36e9fe15d2dce5317fc0b930c53b7e96b1cbff7
vikashgitkit <vikashofficial9@gmail.com>
2025-07-25 22:41:42 +0530

    freez object

--- R2/JAVASCRIPT/02_Basics/3_object.js
+++ R2/JAVASCRIPT/02_Basics/3_object.js
@@ -2,15 +2,25 @@
 //we can create as: Object.create
 
 //(2)Object literals
+
+const mySymb = Symbol("key1");
 const jsUser = {
     name: "steve",
+    "full name": "Steve bow",
     age: 17,
+    [mySymb]: "myKey1",
     location: 'Jaipur',
     email: 'steve@gmail.com',
     isLoggedIn: false,
     lastLoginDays: ["Mon", "Sat"],
 }
 
-console.log(jsUser.email);//Traditional way to access obj values but this is not good way
-console.log(jsUser["email"]);//This is good way to access obj values. "" me key isliye dena pdta he kuki js behind the scens key ko bhi string me hi convert krta he
+console.log(jsUser.email);//Traditional way to access obj values but this is not good way bcz now you can't access "full name" key
+console.log(jsUser["full name"]);//This is good way to access obj values. "" me key isliye dena pdta he kuki js behind the scens key ko bhi string me hi convert krta he
+
+console.log(jsUser[mySymb]);
+
+jsUser.email = "ben@gmail.com";//updating value in obj
+Object.freeze(jsUser);//ab app object ki value me changes nhi kar payenge
+
 

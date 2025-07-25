c9b34c1cd5c663bd09ac25e4189b52ff71114b30
vikashgitkit <vikashofficial9@gmail.com>
2025-07-25 21:29:59 +0530

    array inside the array

---
+++ R2/JAVASCRIPT/02_Basics/2_array.js
@@ -0,0 +1,9 @@
+const marvel_heros = ['thor', "Ironman", "Spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
+
+marvel_heros.push(dc_heros);
+console.log(marvel_heros);//[ 'thor', 'Ironman', 'Spiderman', [ 'superman', 'flash', 'batman' ] ]
+
+//Now if you need to access another array value of an array suppose 'flash'
+console.log(marvel_heros[3][1]);//flash
+

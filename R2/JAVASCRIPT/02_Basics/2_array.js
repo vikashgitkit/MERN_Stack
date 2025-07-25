42f97bbf41f3be56611bb4996d9ba07b4e1c914c
vikashgitkit <vikashofficial9@gmail.com>
2025-07-25 21:34:35 +0530

    concat

--- R2/JAVASCRIPT/02_Basics/2_array.js
+++ R2/JAVASCRIPT/02_Basics/2_array.js
@@ -1,9 +1,13 @@
 const marvel_heros = ['thor', "Ironman", "Spiderman"];
 const dc_heros = ["superman", "flash", "batman"];
 
-marvel_heros.push(dc_heros);
-console.log(marvel_heros);//[ 'thor', 'Ironman', 'Spiderman', [ 'superman', 'flash', 'batman' ] ]
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);//[ 'thor', 'Ironman', 'Spiderman', [ 'superman', 'flash', 'batman' ] ]
 
 //Now if you need to access another array value of an array suppose 'flash'
-console.log(marvel_heros[3][1]);//flash
+// console.log(marvel_heros[3][1]);//flash
+
+const newArr = marvel_heros.concat(dc_heros);
+console.log(newArr);//concat merges two array and return new array
+
 

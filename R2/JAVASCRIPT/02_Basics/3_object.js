cb0fd04495cc4a952cc98aba1c2e2ac96234fe50
vikashgitkit <vikashofficial9@gmail.com>
2025-07-25 22:53:35 +0530

    function in object

--- R2/JAVASCRIPT/02_Basics/3_object.js
+++ R2/JAVASCRIPT/02_Basics/3_object.js
@@ -14,7 +14,7 @@ const jsUser = {
     isLoggedIn: false,
     lastLoginDays: ["Mon", "Sat"],
 }
-
+/*
 console.log(jsUser.email);//Traditional way to access obj values but this is not good way bcz now you can't access "full name" key
 console.log(jsUser["full name"]);//This is good way to access obj values. "" me key isliye dena pdta he kuki js behind the scens key ko bhi string me hi convert krta he
 
@@ -22,5 +22,21 @@ console.log(jsUser[mySymb]);
 
 jsUser.email = "ben@gmail.com";//updating value in obj
 Object.freeze(jsUser);//ab app object ki value me changes nhi kar payenge
+jsUser.age = 22;//this value will not update as we have freez object
+console.log(jsUser);
+*/
+
+jsUser.greeting = function() {
+    console.log("Hello Steve");
+    
+}
+jsUser.greetingTwo = function() {
+    console.log(`Hello second Steve you age is ${this.age}`); //using this keyword bcz access the same obj key
+    
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
+
+
 
 

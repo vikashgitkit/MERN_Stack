d5d597240f19d944779a0d353b10452417eebb80
vikashgitkit <vikashofficial9@gmail.com>
2025-07-26 22:33:47 +0530

    merge objects

--- R2/JAVASCRIPT/02_Basics/4_object.js
+++ R2/JAVASCRIPT/02_Basics/4_object.js
@@ -18,3 +18,8 @@ const regularUser = {
 
 console.log(regularUser.fullname.userfullname.lastname);
 
+const obj1 = {1: 'a', 2: 'b'}
+const obj2 = {3: 'c', 4: 'd'}
+const obj3 = Object.assign({}, obj1, obj2);
+console.log("🚀 ~ obj3:", obj3)// { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+

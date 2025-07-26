1c5dca24ddd8d1c7cd2a2306950f73baee5847ac
vikashgitkit <vikashofficial9@gmail.com>
2025-07-26 22:28:12 +0530

    nested objects

--- R2/JAVASCRIPT/02_Basics/4_object.js
+++ R2/JAVASCRIPT/02_Basics/4_object.js
@@ -5,3 +5,16 @@ tinderUser.name = 'Steve';
 tinderUser.isLoggedIn = false;
 
 console.log(tinderUser);
+
+const regularUser = {
+    email: "abc@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "steve",
+            lastname: "bow"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.lastname);
+

ee19aa27b253983abe89790a8a0cf88a08f70e2b
vikashgitkit <vikashofficial9@gmail.com>
2025-07-26 22:43:02 +0530

    object methods

--- R2/JAVASCRIPT/02_Basics/4_object.js
+++ R2/JAVASCRIPT/02_Basics/4_object.js
@@ -20,6 +20,15 @@ console.log(regularUser.fullname.userfullname.lastname);
 
 const obj1 = {1: 'a', 2: 'b'}
 const obj2 = {3: 'c', 4: 'd'}
-const obj3 = Object.assign({}, obj1, obj2);
+const obj3 = Object.assign({}, obj1, obj2);//But this is not much used instead spread op is used more.
 console.log("🚀 ~ obj3:", obj3)// { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
 
+const obj4 = {...obj1, ...obj2}
+console.log("🚀 ~ obj4:", obj4)// { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+console.log(Object.keys(tinderUser));//[ 'id', 'name', 'isLoggedIn' ]
+
+console.log(Object.values(tinderUser));//[ '123abc', 'Steve', false ]
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));//true

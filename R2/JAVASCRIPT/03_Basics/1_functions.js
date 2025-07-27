e56acb6a4c99add2fb299059b416df7fb046e33a
vikashgitkit <vikashofficial9@gmail.com>
2025-07-27 10:43:57 +0530

    arguments in functions

--- R2/JAVASCRIPT/03_Basics/1_functions.js
+++ R2/JAVASCRIPT/03_Basics/1_functions.js
@@ -6,7 +6,21 @@ function sum(num1, num2){
 let result = sum(2,9); //2 and 9 are arguments
 console.log("Result is:", result);
 
-function loginUserMessage(username){
+//sam is default value like in fn calling if usrname is not passed then it will take sam in usrname
+function loginUserMessage(username = 'sam'){
+    // if(username === undefined){
+    //     console.log("Please enter a username");
+    //     return
+        
+    // }
+    //----------OR-----------
+    if(!username){
+        console.log("Please enter a username");
+        return
+        
+    }
     return `${username} just logged in`
 }
-console.log(loginUserMessage('Steve'));
+// console.log(loginUserMessage('Steve'));
+console.log(loginUserMessage());
+

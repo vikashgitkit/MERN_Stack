526ae34055e297c745e03df6ad81dfe1ff0e0d6c
vikashgitkit <vikashofficial9@gmail.com>
2025-07-27 12:51:47 +0530

    iife

--- R2/JAVASCRIPT/03_Basics/4_iife.js
+++ R2/JAVASCRIPT/03_Basics/4_iife.js
@@ -1,7 +1,7 @@
 //Immediately invoked function expressions(IIFE)
 
 //jab hme global scope ke polution se bachna ho to hm fn ko turant hi call krdete he lekin isko end krne ke liye ; lgana jruri he tabhi niche ka iife fn execute hoga
-(function key() {
+(function key() { //this is named iife bcz key is name
   console.log(`DB connected`);
   
 })();
@@ -9,4 +9,10 @@
 (() => {
     console.log(`DB Connected Two`);
     
-})()
+})();
+
+//passing parameter in iife
+((name) => {
+    console.log(`DB connected ${name}`);
+    
+})('Three');

561954eca81f85084a41f60134324aafbab7c484
vikashgitkit <vikashofficial9@gmail.com>
2025-07-28 16:48:28 +0530

    Map

--- R2/JAVASCRIPT/05_iterations/3_higher_order_array_loops.js
+++ R2/JAVASCRIPT/05_iterations/3_higher_order_array_loops.js
@@ -2,6 +2,33 @@
 const arr = [1,2,3,4,5,6];
 
 for (const num of arr){
-    console.log(num);
+    // console.log(num);
+    
+}
+
+const greetings = "Hello Steve"
+for (const greet of greetings){
+    // console.log(`Each char is ${greet}`);
+    
+}
+
+//Map
+const map = new Map()//map only sets unique key values. as in below we have set india 2 times but it will only set 1 time
+map.set('IN', 'India')
+map.set('US', 'United states of america')
+map.set('FR', 'France')
+map.set('IN', 'India')//this will not set
+
+console.log(map); //op: 
+// Map(3) {
+//   'IN' => 'India',
+//   'US' => 'United states of america',
+//   'FR' => 'France'
+// }
+
+for(const key of map){
+    console.log(key); //op: [ 'IN', 'India' ]
+// [ 'US', 'United states of america' ]
+// [ 'FR', 'France' ]
     
 }

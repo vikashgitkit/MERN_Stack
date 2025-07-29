f040d4ad1c2fbb08d31bedb493b40f713e5b24c5
vikashgitkit <vikashofficial9@gmail.com>
2025-07-29 17:35:43 +0530

    reduce method

--- R2/JAVASCRIPT/05_iterations/6_reduce.js
+++ R2/JAVASCRIPT/05_iterations/6_reduce.js
@@ -9,3 +9,27 @@ const myNums = [1, 2, 3]
 const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 
 console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);

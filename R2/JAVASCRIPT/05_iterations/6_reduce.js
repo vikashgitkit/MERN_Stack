126b36454444b933c681aaf82358d38e94101702
vikashgitkit <vikashofficial9@gmail.com>
2025-07-29 17:36:43 +0530

    reduce method

--- R2/JAVASCRIPT/05_iterations/6_reduce.js
+++ R2/JAVASCRIPT/05_iterations/6_reduce.js
@@ -33,3 +33,5 @@ const shoppingCart = [
 const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 
 console.log(priceToPay);
+
+//++++++++++++++++++++++Basics are complted here+++++++++++++++++//

4ca13485a1a603522b1ce407b892a21c9d318829
vikashgitkit <vikashofficial9@gmail.com>
2025-08-05 12:04:11 +0530

    validating guess

--- R2/JAVASCRIPT/07_Projects/4_guessNumber/index.js
+++ R2/JAVASCRIPT/07_Projects/4_guessNumber/index.js
@@ -1,7 +1,7 @@
 const randNum = parseInt(Math.random() * 100 + 1);
 
 const submit = document.querySelector("#subt")
-const usrInput = parseInt(document.querySelector('#guessField').value);
+const usrInput = document.querySelector('#guessField');
 const guessSlot = document.querySelector('.guesses');
 const remainGuess = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi')
@@ -16,12 +16,26 @@ let playGame = true
 if(playGame){
     submit.addEventListener('click', function (e) {
         e.preventDefault();//value ko server par jane se rokta he
-        const guess = usrInput;
+        const guess = parseInt(usrInput.value);
+        console.log("🚀 ~ guess:", guess)
         validateGuess(guess);
     })
 }
 
 function validateGuess(guess){
+    if(isNaN(guess) || guess < 1 || guess > 100){
+        alert('Please enter a valid number')
+    } else{
+        prevGuess.push(guess)
+        if(numGuess === 11){
+            displayGuess(guess)
+            displayMsg(`Game over. Random number was ${randNum}`)
+            endGame()
+        } else{
+            displayGuess(guess)
+            checkGuess(guess)
+        }
+    }
 
 }
 

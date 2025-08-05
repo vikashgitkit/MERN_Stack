61b519d78d9decaaa1d39f8b286a71928941e919
vikashgitkit <vikashofficial9@gmail.com>
2025-08-05 11:37:57 +0530

    getting elements

---
+++ R2/JAVASCRIPT/07_Projects/4_guessNumber/index.js
@@ -0,0 +1,14 @@
+const randNum = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector("#subt")
+const usrInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remainGuess = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas')
+
+const p = document.createElement('p');
+
+let prevGuess = []
+let numGuess = 1
+let playGame = true

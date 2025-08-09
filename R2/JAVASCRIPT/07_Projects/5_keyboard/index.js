26d77fa25f76b210c165a5ea8f1e55ac31d11f3f
vikashgitkit <vikashofficial9@gmail.com>
2025-08-09 10:10:51 +0530

    integrate the table

--- R2/JAVASCRIPT/07_Projects/5_keyboard/index.js
+++ R2/JAVASCRIPT/07_Projects/5_keyboard/index.js
@@ -5,19 +5,14 @@ window.addEventListener('keydown', (e) => {
       <div class='color'>
       <table>
   <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
+    <th>Key</th>
+    <th>KeyCode</th>
+    <th>Code</th>
   </tr>
   <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
+    <td>${e.key === '' ? "Space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
   </tr>
 </table>
 </div>

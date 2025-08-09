f01d79907d3c0a6253ef0624b1072a7ed7944703
vikashgitkit <vikashofficial9@gmail.com>
2025-08-09 10:06:18 +0530

    adding table

---
+++ R2/JAVASCRIPT/07_Projects/5_keyboard/index.js
@@ -0,0 +1,25 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+      <div class='color'>
+      <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+</div>
+    `
+})

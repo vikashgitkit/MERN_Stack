8a92374021fbf888daf0da40ca5e1794f7f98e52
vikashgitkit <vikashofficial9@gmail.com>
2025-08-09 09:54:53 +0530

    clear interval

--- R2/JAVASCRIPT/07_Projects/6_UnlimitedColors/index.js
+++ R2/JAVASCRIPT/07_Projects/6_UnlimitedColors/index.js
@@ -8,15 +8,20 @@ const randomColor = function() {
     return color;
 }
 
+let intervalId;
 const startChangingColor = function() {
-    setInterval(changeBgColor, 1000)
+   intervalId = setInterval(changeBgColor, 1000)
 
     function changeBgColor() {
         document.body.style.backgroundColor = randomColor();
     }
 }
 
-const stopChangingColor = function() {}
+const stopChangingColor = function() {
+    clearInterval(intervalId);
+    intervalId = null;
+
+}
 
 document.querySelector('#start').addEventListener('click', startChangingColor)
 document.querySelector('#stop').addEventListener('click', stopChangingColor)

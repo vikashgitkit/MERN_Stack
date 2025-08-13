d0e31985c43f39675915217b5310a58cd6c71d09
vikashgitkit <vikashofficial9@gmail.com>
2025-08-13 19:47:15 +0530

    promise hell

--- R2/JAVASCRIPT/09_Advance_one/2_promises.js
+++ R2/JAVASCRIPT/09_Advance_one/2_promises.js
@@ -40,3 +40,27 @@ promiseThree.then(function(user){
     console.log(user);
     
 })
+
+//================================================//
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let err = false
+        if(!err){
+            resolve({username: "Bob", password: 123})
+        } else {
+            reject("Error: Something went wrong")
+        }
+    }, 1000)
+})
+
+//Promise hell jisme hm multiple then ka use krte he and jo first bale then se return hota he use dusre then me pass krte he.
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username;
+}).then((username) => {
+    console.log(username);
+    
+}).catch(function(err) {
+    console.log(err);
+    
+})

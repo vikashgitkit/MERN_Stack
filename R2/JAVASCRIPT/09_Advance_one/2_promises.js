b90e7dcac86dca3b76e8bd9680eacb9bfcbf653a
vikashgitkit <vikashofficial9@gmail.com>
2025-08-13 19:51:45 +0530

    use finally

--- R2/JAVASCRIPT/09_Advance_one/2_promises.js
+++ R2/JAVASCRIPT/09_Advance_one/2_promises.js
@@ -1,66 +1,64 @@
 //Promises ko create krte he and consume(using fetch) krte he)
 
 //(1)creating promise
-const promiseOne = new Promise(function(resolve, reject) {
-    //Do an async task
-    //ex: DB calls, cryptography, network calls etc
+const promiseOne = new Promise(function (resolve, reject) {
+  //Do an async task
+  //ex: DB calls, cryptography, network calls etc
 
-    setTimeout(function() {
-        console.log('Async task is complete');
-        resolve()
-    }, 1000)
-})
+  setTimeout(function () {
+    console.log("Async task is complete");
+    resolve();
+  }, 1000);
+});
 
 //(2)Consuming the promise
-promiseOne.then(function(){
-    console.log('Promise consumed');
-    
-})
+promiseOne.then(function () {
+  console.log("Promise consumed");
+});
 
 //Sorter way to create and consume promise
-new Promise(function(resolve, reject) {
-    setTimeout(function() {
-        console.log("Async task 2");
-        resolve();
-        
-    }, 1000)
-}).then(function() {
-    console.log("Async 2 resolved");
-    
-})
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
 //===============================================//
 
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function() {
-        resolve({username: "Steve", email: "steve@gmail.com"})
-    }, 1000)
-})
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "Steve", email: "steve@gmail.com" });
+  }, 1000);
+});
 
-promiseThree.then(function(user){
-    console.log(user);
-    
-})
+promiseThree.then(function (user) {
+  console.log(user);
+});
 
 //================================================//
-const promiseFour = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let err = false
-        if(!err){
-            resolve({username: "Bob", password: 123})
-        } else {
-            reject("Error: Something went wrong")
-        }
-    }, 1000)
-})
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let err = false;
+    if (!err) {
+      resolve({ username: "Bob", password: 123 });
+    } else {
+      reject("Error: Something went wrong");
+    }
+  }, 1000);
+});
 
 //Promise hell jisme hm multiple then ka use krte he and jo first bale then se return hota he use dusre then me pass krte he.
-promiseFour.then((user) => {
-    console.log(user);
+promiseFour
+  .then((user) => {
+    console.log("User:", user);
     return user.username;
-}).then((username) => {
-    console.log(username);
-    
-}).catch(function(err) {
+  })
+  .then((username) => {
+    console.log("Username:", username);
+  })
+  .catch(function (err) {
     console.log(err);
-    
-})
+  }).finally(() => console.log("The promise is either resolved or rejected")
+  )

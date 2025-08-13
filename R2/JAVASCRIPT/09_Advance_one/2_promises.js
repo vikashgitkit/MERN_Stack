7c7c53216f95f51270bbc452c6bb77438951febb
vikashgitkit <vikashofficial9@gmail.com>
2025-08-13 18:58:24 +0530

    sorter way to create and consume promise

--- R2/JAVASCRIPT/09_Advance_one/2_promises.js
+++ R2/JAVASCRIPT/09_Advance_one/2_promises.js
@@ -16,3 +16,15 @@ promiseOne.then(function(){
     console.log('Promise consumed');
     
 })
+
+//Sorter way to create and consume promise
+new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        console.log("Async task 2");
+        resolve();
+        
+    }, 1000)
+}).then(function() {
+    console.log("Async 2 resolved");
+    
+})

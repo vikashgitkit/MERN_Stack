6cd052eabfa11aa96000a25fabe88e32040be7be
vikashgitkit <vikashofficial9@gmail.com>
2025-08-14 17:38:46 +0530

    constructor

--- R2/JAVASCRIPT/10_classes_oops/1_oops.js
+++ R2/JAVASCRIPT/10_classes_oops/1_oops.js
@@ -1,3 +1,4 @@
+//Object literal
 const user = {
     username: "steve",
     loginCount: 8,
@@ -16,10 +17,24 @@ console.log(user.getUserDetails());
 // console.log(this);
 
 
-function user(username, loginCount, isLoggedIn){
+//Constructor function
+function users(username, loginCount, isLoggedIn){
     this.username = username;
     this.loginCount = loginCount;
     this.isLoggedIn = isLoggedIn;
 
+
+    this.greeting = function() {
+        console.log(`Welcome ${this.username}`);
+        
+    }
     return this
 }
+
+const userOne = new users("Bob",12, true)
+const userTwo = new users("Cavin",55, false)
+// console.log(userOne);
+console.log(userOne.construcor);
+console.log(userTwo);
+
+

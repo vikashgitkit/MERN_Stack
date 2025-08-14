92aae0dd5c6ffe05319829aa0e7f463d059af553
vikashgitkit <vikashofficial9@gmail.com>
2025-08-14 19:04:25 +0530

    inheritance

--- R2/JAVASCRIPT/10_classes_oops/3_prototype.js
+++ R2/JAVASCRIPT/10_classes_oops/3_prototype.js
@@ -27,3 +27,27 @@ Array.prototype.heyHitesh = function(){
 // myHeros.hitesh()
 // myHeros.heyHitesh()
 // heroPower.heyHitesh()
+
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User

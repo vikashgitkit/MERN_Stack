e75d6f9b3473d44cec611c9ceb746d1466f037f7
vikashgitkit <vikashofficial9@gmail.com>
2025-08-14 19:02:49 +0530

    Prototype

--- R2/JAVASCRIPT/10_classes_oops/3_prototype.js
+++ R2/JAVASCRIPT/10_classes_oops/3_prototype.js
@@ -5,3 +5,25 @@
 
 
 let myHeros = ["thor", "spiderman"]
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.hitesh = function(){
+    console.log(`hitesh is present in all objects`);
+}
+
+Array.prototype.heyHitesh = function(){
+    console.log(`Hitesh says hello`);
+}
+
+// heroPower.hitesh()
+// myHeros.hitesh()
+// myHeros.heyHitesh()
+// heroPower.heyHitesh()

51fa44e9e4c7bd5202fa0d26df5b88c98792974d
vikashgitkit <vikashofficial9@gmail.com>
2025-08-14 14:58:45 +0530

    object in oops

---
+++ R2/JAVASCRIPT/10_classes_oops/1_oops.js
@@ -0,0 +1,14 @@
+const user = {
+    username: "steve",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        console.log("Got user details from database");
+        
+    }
+}
+
+console.log(user.username);
+console.log(user.getUserDetails());
+

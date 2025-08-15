b79a0437b0c4cedf5788421da16aee8e8b851aee
vikashgitkit <vikashofficial9@gmail.com>
2025-08-15 17:04:29 +0530

    call method

--- R2/JAVASCRIPT/10_classes_oops/4_call.js
+++ R2/JAVASCRIPT/10_classes_oops/4_call.js
@@ -1,11 +1,16 @@
 function setUsername(username){
     //complex DB calls
     this.username = username
+    console.log("Called");
+    
 }
 
 function createUser(username, email, password){
-    setUsername(username)
+    setUsername.call(this, username)
 
     this.email = email
     this.password = password
 }
+
+const chai = new createUser("Chai", "chai@gmail.com", 123)
+console.log(chai);

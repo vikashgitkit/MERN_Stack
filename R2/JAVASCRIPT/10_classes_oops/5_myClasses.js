86c6de6f9eebb3c8ea146844f299c7a9f8eb7704
vikashgitkit <vikashofficial9@gmail.com>
2025-08-15 17:20:05 +0530

    classes

--- R2/JAVASCRIPT/10_classes_oops/5_myClasses.js
+++ R2/JAVASCRIPT/10_classes_oops/5_myClasses.js
@@ -22,3 +22,24 @@ console.log(usr.encryptPassword());
 console.log(usr.changeUsername());
 
 
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
+

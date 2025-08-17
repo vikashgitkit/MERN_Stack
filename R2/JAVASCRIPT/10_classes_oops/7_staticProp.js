28d21bf460dd18f20eb732d5fbe521f05a75e680
vikashgitkit <vikashofficial9@gmail.com>
2025-08-17 18:05:48 +0530

    static

---
+++ R2/JAVASCRIPT/10_classes_oops/7_staticProp.js
@@ -0,0 +1,17 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe() {
+        console.log(`username: ${this.username}`);
+        
+    }
+//static keyword object ke reference ko access krne se rok dega
+    static createId() {
+        return `123`
+    }
+}
+
+const user = new User("Steve")
+console.log(user.createId);//jesa ki user User object ka reference he or mene createId par static use kiya he to ye access allow nhi krta he error dega

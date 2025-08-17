9c0382bcda7b561ff019789d980212c4b9a0d2ae
vikashgitkit <vikashofficial9@gmail.com>
2025-08-17 17:48:47 +0530

    super keyword

--- R2/JAVASCRIPT/10_classes_oops/6_inheritance.js
+++ R2/JAVASCRIPT/10_classes_oops/6_inheritance.js
@@ -8,3 +8,16 @@ class User {
         
     }
 }
+
+class Teacher extends User{
+    constructor(username, email, password) {
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse() {
+        console.log(`A new course was added by ${this.username}`);
+        
+    }
+}

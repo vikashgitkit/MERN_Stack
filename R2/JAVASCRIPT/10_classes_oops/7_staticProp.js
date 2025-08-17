e7eada93e58a876be4205629407055fb5c8cb1c9
vikashgitkit <vikashofficial9@gmail.com>
2025-08-17 18:11:05 +0530

    static

--- R2/JAVASCRIPT/10_classes_oops/7_staticProp.js
+++ R2/JAVASCRIPT/10_classes_oops/7_staticProp.js
@@ -14,4 +14,15 @@ class User {
 }
 
 const user = new User("Steve")
-console.log(user.createId);//jesa ki user User object ka reference he or mene createId par static use kiya he to ye access allow nhi krta he error dega
+// console.log(user.createId);//jesa ki user User object ka reference he or mene createId par static use kiya he to ye access allow nhi krta he error dega
+
+
+class Teacher extends User{
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const teacher = Teacher("Bob", "bob@gmail.com")
+console.log(teacher.createId())

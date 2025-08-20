fde7d490e67112dc505c858d704c3e44f1ce6413
vikashgitkit <vikashofficial9@gmail.com>
2025-08-20 19:12:50 +0530

    dynamic component

--- R1/REACTJS/2_create_react_comp/src/DynamicComp.jsx
+++ R1/REACTJS/2_create_react_comp/src/DynamicComp.jsx
@@ -2,7 +2,15 @@ export function DynamicComp() {
 
     let myName = 'Steve';
 
-    return <h3>
+    let fullName = () => {
+        return 'Alice bob'
+    }
+
+    return <div>
+    <h3>
         Hello this is the future speaking. I am {myName}
     </h3>
+    <h2> My full name is: {fullName()}</h2>
+
+    </div>
 }

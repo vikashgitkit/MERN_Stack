b40614b99f52ad09fcb97136aa28b9867a927edb
vikashgitkit <vikashofficial9@gmail.com>
2025-08-20 18:08:14 +0530

    added button component

--- R1/REACTJS/2_create_react_comp/src/App.jsx
+++ R1/REACTJS/2_create_react_comp/src/App.jsx
@@ -1,5 +1,12 @@
+import KgButton from "./KgButton";
+
 function App() {
-  return <h1>Hello component</h1>;
+  return <div>
+    <h1>Hello component</h1>
+    <KgButton></KgButton>
+  </div>
 }
 
 export default App;
+
+//website where we can see how jsx is converted into js: https://babeljs.io/repl
---
+++ R1/REACTJS/2_create_react_comp/src/KgButton.jsx
@@ -0,0 +1,5 @@
+function KgButton() {
+    return <button>Click Me!</button>
+}
+
+export default KgButton;

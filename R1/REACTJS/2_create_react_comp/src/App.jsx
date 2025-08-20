d5ae82a6a16be997b367aba6d2d136ec19679ad4
vikashgitkit <vikashofficial9@gmail.com>
2025-08-20 19:00:49 +0530

    dynamic component

--- R1/REACTJS/2_create_react_comp/src/App.jsx
+++ R1/REACTJS/2_create_react_comp/src/App.jsx
@@ -1,11 +1,13 @@
 import { Exports } from "./Export"; //if componenet is not default export then we need to import comp inside {}
 import KgButton from "./KgButton";
+import { DynamicComp } from "./DynamicComp";
 
 function App() {
   return <div>
     <h1>Hello component</h1>
     <KgButton></KgButton>
     <Exports></Exports>
+    <DynamicComp/>
   </div>
 }
 
---
+++ R1/REACTJS/2_create_react_comp/src/DynamicComp.jsx
@@ -0,0 +1,8 @@
+export function DynamicComp() {
+
+    let myName = 'Steve';
+
+    return <h3>
+        Hello this is the future speaking. I am {myName}
+    </h3>
+}

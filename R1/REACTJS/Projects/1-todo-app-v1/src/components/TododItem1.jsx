0c4c3b397f9c080961cf08006de40872e98feed2
vikashgitkit <vikashofficial9@gmail.com>
2025-08-21 17:23:07 +0530

    Adding custom css

--- R1/REACTJS/Projects/1-todo-app-v1/src/components/TododItem1.jsx
+++ R1/REACTJS/Projects/1-todo-app-v1/src/components/TododItem1.jsx
@@ -3,12 +3,12 @@ function TodoItem1() {
     let todoName = "Buy Milk";
     let todoDate = "21/08/2025"
   return (
-    <div class="container text-center">
-      <div class="row">
+    <div class="container">
+      <div class="row kg-row">
         <div class="col-4">{todoName}</div>
         <div class="col-4">{todoDate}</div>
         <div class="col-2">
-          <button type="button" class="btn btn-danger">
+          <button type="button" class="btn btn-danger kg-button">
             Delete
           </button>
         </div>

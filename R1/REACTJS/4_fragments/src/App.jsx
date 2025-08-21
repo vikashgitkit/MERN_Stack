0b9a84e245b58377423cea051d14d37c34c183a7
vikashgitkit <vikashofficial9@gmail.com>
2025-08-21 18:55:53 +0530

    Map method

--- R1/REACTJS/4_fragments/src/App.jsx
+++ R1/REACTJS/4_fragments/src/App.jsx
@@ -1,20 +1,18 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
-  return <>
-
-   <h1>Healthy Foods</h1>
-   <ul class="list-group">
-  <li class="list-group-item">Ghee</li>
-  <li class="list-group-item">Salad</li>
-  <li class="list-group-item">Milk</li>
-  <li class="list-group-item">Daal</li>
-  <li class="list-group-item">Roti</li>
-</ul>
-  </>
-  
+  let foodItems = ["Dal","Salad","Ghee","Milk", "Curd"]
+  return (
+    <>
+      <h1>Healthy Foods</h1>
+      <ul className="list-group">
+      {foodItems.map((item) => (
+        <li className="list-group-item">{item}</li>
+      ))}
+      </ul>
+    </>
+  );
 }
 
-export default App
+export default App;

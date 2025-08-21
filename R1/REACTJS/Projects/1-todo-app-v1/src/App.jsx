4578b7a6120c61b05376956dcd720e180e2bcb0e
vikashgitkit <vikashofficial9@gmail.com>
2025-08-21 12:49:03 +0530

    created AppName and AddTodo componenets

--- R1/REACTJS/Projects/1-todo-app-v1/src/App.jsx
+++ R1/REACTJS/Projects/1-todo-app-v1/src/App.jsx
@@ -1,20 +1,13 @@
+import AddTodo from "./components/AddTodo";
+import AppName from "./components/AppName";
+
 function App() {
   return (
     <center className="todo-container">
-      <h1>TODO APP</h1>
+    <AppName />
 
       <div class="container text-center">
-        <div class="row">
-          <div class="col-4">
-            <input type="text" placeholder="Enter Todo Here" />
-          </div>
-          <div class="col-4">
-            <input type="date" />
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-success">Success</button>
-          </div>
-        </div>
+        <AddTodo/>
 
 
         <div class="row">
---
+++ R1/REACTJS/Projects/1-todo-app-v1/src/components/AddTodo.jsx
@@ -0,0 +1,19 @@
+function AddTodo() {
+  return (
+    <div class="row">
+      <div class="col-4">
+        <input type="text" placeholder="Enter Todo Here" />
+      </div>
+      <div class="col-4">
+        <input type="date" />
+      </div>
+      <div class="col-2">
+        <button type="button" class="btn btn-success">
+          Add
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodo

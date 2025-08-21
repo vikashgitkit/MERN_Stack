e4dddd6dd3b6c808594a88a112d1fee9d37b3973
vikashgitkit <vikashofficial9@gmail.com>
2025-08-21 12:34:12 +0530

    creating input type and add todo button using bootstrap layout

--- R1/REACTJS/Projects/1-todo-app-v1/src/App.jsx
+++ R1/REACTJS/Projects/1-todo-app-v1/src/App.jsx
@@ -1,7 +1,23 @@
 function App() {
-  return <center className="todo-container">
-    <h1>TODO APP</h1>
-  </center>
+  return (
+    <center className="todo-container">
+      <h1>TODO APP</h1>
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-4">
+            <input type="text" placeholder="Enter Todo Here" />
+          </div>
+          <div class="col-4">
+            <input type="date" />
+          </div>
+          <div class="col-2">
+            <button>Add</button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;

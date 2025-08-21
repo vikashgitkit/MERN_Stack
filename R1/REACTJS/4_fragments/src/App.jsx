3b24ecc163b9470c7ac911feb7be9747814f744e
vikashgitkit <vikashofficial9@gmail.com>
2025-08-21 18:46:08 +0530

    react fragment

---
+++ R1/REACTJS/4_fragments/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return <>
+
+   <h1>Healthy Foods</h1>
+   <ul class="list-group">
+  <li class="list-group-item">Ghee</li>
+  <li class="list-group-item">Salad</li>
+  <li class="list-group-item">Milk</li>
+  <li class="list-group-item">Daal</li>
+  <li class="list-group-item">Roti</li>
+</ul>
+  </>
+  
+}
+
+export default App

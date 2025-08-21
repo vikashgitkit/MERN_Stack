0abf2c067a24e5e6e13f3a70bb2c700393be4aa5
vikashgitkit <vikashofficial9@gmail.com>
2025-08-21 12:41:21 +0530

    added another text and delete button

--- R1/REACTJS/Projects/1-todo-app-v1/src/App.jsx
+++ R1/REACTJS/Projects/1-todo-app-v1/src/App.jsx
@@ -28,6 +28,20 @@ function App() {
             <button type="button" class="btn btn-danger">Delete</button>
           </div>
         </div>
+
+
+
+        <div class="row">
+          <div class="col-4">
+            Go To College
+          </div>
+          <div class="col-4">
+            26/08/2025
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </div>
       </div>
     </center>
   );

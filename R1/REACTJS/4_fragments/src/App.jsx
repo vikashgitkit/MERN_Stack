e50242df90c5098abc63f5d5dd8d9c10baf5954c
vikashgitkit <vikashofficial9@gmail.com>
2025-08-22 21:17:17 +0530

    put ternary inside component

--- R1/REACTJS/4_fragments/src/App.jsx
+++ R1/REACTJS/4_fragments/src/App.jsx
@@ -3,12 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   // let foodItems = ["Dal","Salad","Ghee","Milk", "Curd"]
-
   let foodItems = []
+
+  // if(foodItems.length === 0){
+  //   return <h3>I am still hungry</h3>
+  // }
+
+  let emptyMsg = foodItems.length === 0 ? <h3>I am still hungry</h3> : null;
   return (
     <>
       <h1>Healthy Foods</h1>
-      {foodItems.length === 0 ? <h3>I am still hungry</h3> : null}
+      {/* {foodItems.length === 0 ? <h3>I am still hungry</h3> : null} */}
+      {emptyMsg}
       <ul className="list-group">
       {foodItems.map((item) => (
         <li key={item} className="list-group-item">{item}</li>

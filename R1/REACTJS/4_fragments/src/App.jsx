d676e0f58f8aee5d948e0e6a7ebd06e47049be1f
vikashgitkit <vikashofficial9@gmail.com>
2025-08-22 21:19:54 +0530

    put ternary inside component

--- R1/REACTJS/4_fragments/src/App.jsx
+++ R1/REACTJS/4_fragments/src/App.jsx
@@ -9,12 +9,14 @@ function App() {
   //   return <h3>I am still hungry</h3>
   // }
 
-  let emptyMsg = foodItems.length === 0 ? <h3>I am still hungry</h3> : null;
+  // let emptyMsg = foodItems.length === 0 ? <h3>I am still hungry</h3> : null;
   return (
     <>
       <h1>Healthy Foods</h1>
       {/* {foodItems.length === 0 ? <h3>I am still hungry</h3> : null} */}
-      {emptyMsg}
+      {/* {emptyMsg} */}
+
+      {foodItems.length === 0 && <h3>I am still hungry</h3>}
       <ul className="list-group">
       {foodItems.map((item) => (
         <li key={item} className="list-group-item">{item}</li>

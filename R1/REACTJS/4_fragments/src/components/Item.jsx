396a79719f3531aeb934f1747578e226e465a0f7
vikashgitkit <vikashofficial9@gmail.com>
2025-08-24 19:37:26 +0530

    css module

--- R1/REACTJS/4_fragments/src/components/Item.jsx
+++ R1/REACTJS/4_fragments/src/components/Item.jsx
@@ -3,7 +3,7 @@ import styles from './Item.module.css'
 const Item = ({foodItem}) => {
   return (
     <li className= {`${styles["kg-item"]} list-group-item`}>
-      <span className="kg-span">{foodItem}</span>
+      <span className={styles["kg-span"]}>{foodItem}</span>
     </li>
   );
 };

fe5719de3a518e7288362d4e6581422ffb8bf957
vikashgitkit <vikashofficial9@gmail.com>
2025-08-25 15:05:27 +0530

    Created the container component

--- R1/REACTJS/4_fragments/src/App.jsx
+++ R1/REACTJS/4_fragments/src/App.jsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
 import "./App.css"
+import Container from "./components/Container";
 
 function App() {
   let foodItems = ["Dal","Salad","Ghee","Milk", "Curd"]
@@ -13,14 +14,14 @@ function App() {
 
   // let emptyMsg = foodItems.length === 0 ? <h3>I am still hungry</h3> : null;
   return (
-    <>
+    <Container>
       <h1 className="food-heading">Healthy Foods</h1>
       {/* {foodItems.length === 0 ? <h3>I am still hungry</h3> : null} */}
       {/* {emptyMsg} */}
 
      <ErrorMessage items = {foodItems}/>
      <FoodItems items = {foodItems}/>
-    </>
+    </Container>
   );
 }
 

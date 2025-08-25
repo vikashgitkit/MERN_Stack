7e539b1d3ca089af3a0244265a5ce86d765f6b04
vikashgitkit <vikashofficial9@gmail.com>
2025-08-25 14:07:57 +0530

    created the ButtonsContainer component

--- R1/REACTJS/Projects/4-calculator-v1/src/components/ButtonsContainer.jsx
+++ R1/REACTJS/Projects/4-calculator-v1/src/components/ButtonsContainer.jsx
@@ -1,13 +1,32 @@
-import styles from './ButtonsContainer.module.css'
+import styles from "./ButtonsContainer.module.css";
 const ButtonsContainer = () => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
 
-    return  <div className={styles.buttonsContainer}>
-      <button className={styles.button}>C</button>
-      <button className={styles.button}>1</button>
-      <button className={styles.button}>2</button>
-      <button className={styles.button}>+</button>
+  return (
+    <div className={styles.buttonsContainer}>
+    {buttonNames.map((btn)=> (
+      <button className={styles.button}>{btn}</button>
+    ))}
     </div>
+  );
+};
 
-}
-
-export default ButtonsContainer
+export default ButtonsContainer;

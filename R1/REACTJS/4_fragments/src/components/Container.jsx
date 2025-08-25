ab946af97c3961e776e51e23ff268ec91e09a1eb
vikashgitkit <vikashofficial9@gmail.com>
2025-08-25 15:09:43 +0530

    Created the container module

--- R1/REACTJS/4_fragments/src/components/Container.jsx
+++ R1/REACTJS/4_fragments/src/components/Container.jsx
@@ -1,5 +1,6 @@
+import styles from './Container.module.css'
 const Container = (props) => {
-    return <div>{props.children}</div>
+    return <div className={styles.container}>{props.children}</div>
 }
 
 export default Container

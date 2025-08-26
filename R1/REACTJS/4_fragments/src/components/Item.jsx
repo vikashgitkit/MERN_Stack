178e6695103c31bf798f43940f67690c1ac3bd32
vikashgitkit <vikashofficial9@gmail.com>
2025-08-26 21:28:20 +0530

    created separate button for the handle buy

--- R1/REACTJS/4_fragments/src/components/Item.jsx
+++ R1/REACTJS/4_fragments/src/components/Item.jsx
@@ -1,11 +1,15 @@
 import styles from './Item.module.css'
 
 const Item = ({foodItem}) => {
+
+  const handleBuyBtnClicked = (foodItem) => {
+        console.log(`${foodItem} being bought`)
+  }
   return (
     <li className= {`${styles["kg-item"]} list-group-item`}>
       <span className={styles["kg-span"]}>{foodItem}</span>
       <button className={`${styles.btn} btn btn-info`}
-      onClick={() => console.log(`${foodItem} being bought`)}
+      onClick={() => handleBuyBtnClicked(foodItem) }
       >Buy</button>
     </li>
   );

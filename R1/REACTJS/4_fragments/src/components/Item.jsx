f6bab2cb7689f4975f6ac958e0cbad026c3ca466
vikash-astranova <vikash@astranova.world>
2025-08-26 20:14:13 +0530

    Added the button

--- R1/REACTJS/4_fragments/src/components/Item.jsx
+++ R1/REACTJS/4_fragments/src/components/Item.jsx
@@ -4,6 +4,7 @@ const Item = ({foodItem}) => {
   return (
     <li className= {`${styles["kg-item"]} list-group-item`}>
       <span className={styles["kg-span"]}>{foodItem}</span>
+      <button className={`${styles.btn} btn btn-info`}>Buy</button>
     </li>
   );
 };

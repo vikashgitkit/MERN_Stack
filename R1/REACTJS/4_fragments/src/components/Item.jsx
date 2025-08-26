caa1fa0da9bed7515c98b44f1e8994cb6ef4ff49
vikashgitkit <vikashofficial9@gmail.com>
2025-08-26 21:32:23 +0530

    consoled the event object

--- R1/REACTJS/4_fragments/src/components/Item.jsx
+++ R1/REACTJS/4_fragments/src/components/Item.jsx
@@ -2,14 +2,16 @@ import styles from './Item.module.css'
 
 const Item = ({foodItem}) => {
 
-  const handleBuyBtnClicked = (foodItem) => {
+  const handleBuyBtnClicked = (event) => {
+    console.log("Event object is:", event);
+    
         console.log(`${foodItem} being bought`)
   }
   return (
     <li className= {`${styles["kg-item"]} list-group-item`}>
       <span className={styles["kg-span"]}>{foodItem}</span>
       <button className={`${styles.btn} btn btn-info`}
-      onClick={() => handleBuyBtnClicked(foodItem) }
+      onClick={(event) => handleBuyBtnClicked(event) }
       >Buy</button>
     </li>
   );

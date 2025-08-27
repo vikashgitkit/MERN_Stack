6a1c4b46501b722115298629348272fbd2163e53
vikashgitkit <vikashofficial9@gmail.com>
2025-08-27 12:25:17 +0530

    Created the foodInput with its css module

--- R1/REACTJS/4_fragments/src/components/FoodInput.jsx
+++ R1/REACTJS/4_fragments/src/components/FoodInput.jsx
@@ -1,6 +1,11 @@
+import styles from './FoodInput.module.css'
+
 const FoodInput = () => {
 
-    return <input type="text" />
+    return <input type="text" 
+        placeholder='Enter Food Item Here'
+        className={styles.foodInput}
+    />
 }
 
 export default FoodInput

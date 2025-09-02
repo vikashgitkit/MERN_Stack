dede977b7654e0015afb16420fc713089c05c0fe
vikashgitkit <vikashofficial9@gmail.com>
2025-09-02 21:30:09 +0530

    Accessing todoitems

--- R1/REACTJS/Projects/8-todo-app-contextApi-v5/src/components/WelcomeMessage.jsx
+++ R1/REACTJS/Projects/8-todo-app-contextApi-v5/src/components/WelcomeMessage.jsx
@@ -3,7 +3,8 @@ import { useContext } from "react";
 import { TodoItemsContext } from "../store/todo-items-store";
 
 const WelcomeMessage = () => {
-    const todoItems = useContext(TodoItemsContext)
+   const contextObj = useContext(TodoItemsContext)
+  const todoItems = contextObj.todoItems
   
   return todoItems.length === 0 && <p className={styles.welcome}>Enjoy Your Day</p>;
 };

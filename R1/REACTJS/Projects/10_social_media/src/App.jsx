7315f288a219ccae435c3763a1671c77978515df
vikashgitkit <vikashofficial9@gmail.com>
2025-09-07 15:52:04 +0530

    Added sidebar component

--- R1/REACTJS/Projects/10_social_media/src/App.jsx
+++ R1/REACTJS/Projects/10_social_media/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
     <>
+       <Sidebar/>
        <Header/>
        <Footer/>
     </>

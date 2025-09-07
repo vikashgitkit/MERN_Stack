4178837b9c10f41bedac07690601e19924cf9020
vikashgitkit <vikashofficial9@gmail.com>
2025-09-07 15:31:49 +0530

    created header component

--- R1/REACTJS/Projects/10_social_media/src/App.jsx
+++ R1/REACTJS/Projects/10_social_media/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      Placeholder
-       </div>
+       <Header/>
     </>
   )
 }

ff19dcb685e8f268447e568c3e20163f764d3966
vikashgitkit <vikashofficial9@gmail.com>
2025-09-07 17:03:37 +0530

    PostList component

--- R1/REACTJS/Projects/10_social_media/src/App.jsx
+++ R1/REACTJS/Projects/10_social_media/src/App.jsx
@@ -1,24 +1,26 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Sidebar from './components/Sidebar'
-import CreatePost from './components/CreatePost'
-import Post from './components/Post'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import { useState } from "react";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
 
   return (
-    <div className='app-container'>
-       <Sidebar/>
-       <div className='content'>
-       <Header/>
-       <CreatePost/>
-       <Post/>
-       <Footer/>
-       </div>
+    <div className="app-container">
+      <Sidebar />
+      <div className="content">
+        <Header />
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

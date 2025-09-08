6e4f8360062d75c0f5600d65689c61fb72ee173f
vikashgitkit <vikashofficial9@gmail.com>
2025-09-08 19:52:33 +0530

    Created the context

--- R1/REACTJS/Projects/10_social_media/src/store/post-list-store.jsx
+++ R1/REACTJS/Projects/10_social_media/src/store/post-list-store.jsx
@@ -1,8 +1,14 @@
-const { createContext } = require("react");
+const { createContext, useReducer } = require("react");
 
-const PostList = createContext({});
+const PostList = createContext({
+    postList: [],
+    addPost: () => {},
+    deletePost: () => {},
+});
 
 const PostListProvider = ({children}) => {
+    const [postList, dispatchPostList] = useReducer()
+    
     return <PostList.Provider value={[]}>{children}</PostList.Provider>
 
 }
